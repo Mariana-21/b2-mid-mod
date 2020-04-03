c2bf005f1167be9b7894cf973082eaa8ac31ce13
Mariana Cid <cidmariana5@gmail.com>
2020-04-03 10:26:07 -0600

    amusement database created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    mech_1 = Mechanic.create(   name: "Jerry Hill",
+                                    experience: "15 years")
+    mech_2 = Mechanic.create(   name: "Dwight Shrute",
+                                    experience: "10 years")
+    mech_3 = Mechanic.create(   name: "Rachel Green",
+                                    experience: "5 years")
